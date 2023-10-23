3e97dad1caeb7e617e11f8cbf5792bdd6143f6e4
gift56 <efeasiughu@gmail.com>
2023-10-23 18:17:26 -0400

    added images and icons

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -8,6 +8,7 @@ module.exports = {
         dark: "#1A191E",
         navhover: "#F8F8F8",
         bordergray: "#E8E8E8",
+        featureBg: "#E8F2EE",
       },
       fontFamily: {
         dm: "'DM Sans', sans-serif",
