0c7fbebe88401efecf161845f5b5694191c50830
gift56 <efeasiughu@gmail.com>
2023-10-24 15:52:12 -0400

    working on constants file

--- client/src/utils/constant.js
+++ client/src/utils/constant.js
@@ -1,6 +1,52 @@
+import appIcon1 from "../assets/icons/appIcon1.svg";
+import appIcon2 from "../assets/icons/appIcon2.svg";
+import appIcon3 from "../assets/icons/appIcon3.svg";
+import appIcon4 from "../assets/icons/appIcon4.svg";
+import appIcon5 from "../assets/icons/appIcon5.svg";
+import appIcon6 from "../assets/icons/appIcon6.svg";
+
 export const heroFeatures = [
   "Instant Transfer",
   "Payments worldwide",
   "Saving accounts",
   "100% mobile banking",
+];
+
+export const bankingFeature = [
+  {
+    iconSrc: appIcon1,
+    title: "Instant transactions",
+    detail:
+      "Odio euismod lacinia at quis. Amet purus gravida quis blandit turpis.",
+  },
+  {
+    iconSrc: appIcon2,
+    title: "Saving accounts",
+    detail:
+      "Odio euismod lacinia at quis. Amet purus gravida quis blandit turpis.",
+  },
+  {
+    iconSrc: appIcon3,
+    title: "Mobile banking",
+    detail:
+      "Odio euismod lacinia at quis. Amet purus gravida quis blandit turpis.",
+  },
+  {
+    iconSrc: appIcon4,
+    title: "Advanced statistics",
+    detail:
+      "Odio euismod lacinia at quis. Amet purus gravida quis blandit turpis.",
+  },
+  {
+    iconSrc: appIcon5,
+    title: "Virtual cards",
+    detail:
+      "Odio euismod lacinia at quis. Amet purus gravida quis blandit turpis.",
+  },
+  {
+    iconSrc: appIcon6,
+    title: "Contactless payments",
+    detail:
+      "Odio euismod lacinia at quis. Amet purus gravida quis blandit turpis.",
+  },
 ];
