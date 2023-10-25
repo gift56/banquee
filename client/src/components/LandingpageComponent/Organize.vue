f952fa12e698436c8cff7c4331c772fd997aff07
gift56 <efeasiughu@gmail.com>
2023-10-25 17:20:36 -0400

    working on another section

--- client/src/components/LandingpageComponent/Organize.vue
+++ client/src/components/LandingpageComponent/Organize.vue
@@ -1,11 +1,17 @@
 <template>
   <section class="w-full bg-white">
     <div class="container">
-      <div class="w-full flex flex-col items-start justify-start gap-6 py-16">
+      <div class="w-full flex flex-col items-start justify-start gap-4 py-16">
         <h6 class="text-base font-medium text-dark md:text-xl font-dm">
           Saving Accounts
         </h6>
-        <h3 class="headingtext2">Organize your money the right way</h3>
+        <h3 class="headingtext2">
+          Organize your <br class="hidden tab:block" />
+          money the right way
+        </h3>
+        <div class="w-full flex flex-col md:flex-row items-center justify-between gap-6">
+          <p></p>
+        </div>
       </div>
     </div>
   </section>
