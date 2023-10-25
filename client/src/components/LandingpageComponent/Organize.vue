557a2fe8de0503aef1c252db7f11b7992ec97910
gift56 <efeasiughu@gmail.com>
2023-10-25 17:16:08 -0400

    working on another section

--- client/src/components/LandingpageComponent/Organize.vue
+++ client/src/components/LandingpageComponent/Organize.vue
@@ -1,8 +1,8 @@
 <template>
   <section class="w-full bg-white">
     <div class="container">
-      <div class="sectionCon">
-        
+      <div class="w-full flex flex-col items-start justify-start gap-6">
+        <h6>Saving Accounts</h6>
       </div>
     </div>
   </section>
