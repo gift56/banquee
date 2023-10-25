b33869fa8f8263d2ca00ce46dbad84b7db18dd07
gift56 <efeasiughu@gmail.com>
2023-10-25 17:16:53 -0400

    working on another section

--- client/src/components/LandingpageComponent/Organize.vue
+++ client/src/components/LandingpageComponent/Organize.vue
@@ -2,7 +2,9 @@
   <section class="w-full bg-white">
     <div class="container">
       <div class="w-full flex flex-col items-start justify-start gap-6">
-        <h6 class="">Saving Accounts</h6>
+        <h6 class="text-base font-medium text-dark md:text-xl font-dm">
+          Saving Accounts
+        </h6>
       </div>
     </div>
   </section>
