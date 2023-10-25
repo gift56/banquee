22e615b89473231ab46778278033afc048893469
gift56 <efeasiughu@gmail.com>
2023-10-25 16:24:27 -0400

    working on another section

--- client/src/components/LandingpageComponent/SendAndReceive.vue
+++ client/src/components/LandingpageComponent/SendAndReceive.vue
@@ -1,5 +1,14 @@
 <template>
-  <section class="bg-featureBg w-full">Send and Receive</section>
+  <section class="bg-featureBg w-full overflow-hidden">
+    <div class="container">
+      <div class="sectionCon">
+        <div class="flex-1 flex flex-col items-start justify-start gap-6">
+          <h3></h3>
+        </div>
+        <div class="flex-1"></div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script setup></script>
