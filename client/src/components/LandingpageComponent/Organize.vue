d809668eb621de8dc5940c39231dcb4827ea9fbb
gift56 <efeasiughu@gmail.com>
2023-10-25 17:14:53 -0400

    working on another section

--- client/src/components/LandingpageComponent/Organize.vue
+++ client/src/components/LandingpageComponent/Organize.vue
@@ -1,5 +1,11 @@
 <template>
-  <section class="w-full bg-white">Organize</section>
+  <section class="w-full bg-white">
+    <div class="container">
+      <div class="sectionCon">
+        
+      </div>
+    </div>
+  </section>
 </template>
 
 <script setup></script>
