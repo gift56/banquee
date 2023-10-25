1cda8e5845bafd127a181ae4f20bf80f0bcac17a
gift56 <efeasiughu@gmail.com>
2023-10-25 15:59:00 -0400

    working on another section

--- client/src/components/LandingpageComponent/OneBanking.vue
+++ client/src/components/LandingpageComponent/OneBanking.vue
@@ -1,5 +1,11 @@
 <template>
-  <section class="w-full bg-white">One Banking</section>
+  <section class="w-full bg-white">
+    <div class="container">
+      <div class="sectionCon">
+        <div></div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script setup></script>
