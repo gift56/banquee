3d3281d74418b7f8700132cef00a6769e4bd080a
gift56 <efeasiughu@gmail.com>
2023-10-25 15:04:50 -0400

    working on hero section

--- client/src/components/LandingpageComponent/Hero.vue
+++ client/src/components/LandingpageComponent/Hero.vue
@@ -11,7 +11,7 @@
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore.
           </p>
-          <div></div>
+          <div class="w-full grid grid-cols-2 gap-4 items-start"></div>
           <div></div>
         </div>
         <div class="flex-1 relative flex items-center justify-center">
