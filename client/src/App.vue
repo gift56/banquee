b74e1423009aa1cbbce5c11a61c1fb8932f67154
gift56 <efeasiughu@gmail.com>
2023-10-24 20:57:06 -0400

    working on header

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,7 +1,10 @@
 <script setup>
 import { RouterView } from "vue-router";
+import Header from "./components/Header.vue";
 </script>
 
 <template>
-  <RouterView />
+  <Header />
+   <RouterView />
+  <footer></footer>
 </template>
---
+++ client/src/components/Header.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { ref } from "vue";
+
+const showNav = ref(false);
+</script>
+
+<template>
+  <header class="w-full sticky top-0 z-40">
+    <nav class="px-20 py-4 tab:px-40">hdek</nav>
+  </header>
+</template>
