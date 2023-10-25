56d7259c791c09d5a18814a5fcd7de3ead575865
gift56 <efeasiughu@gmail.com>
2023-10-24 21:10:47 -0400

    working on header

--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -13,6 +13,13 @@ const showNav = ref(false);
           banquee.
         </h2>
       </RouterLink>
+      <nav class="flex items-center justify-start gap-6">
+        <RouterLink
+          to="/"
+          class="text-base font-medium text-dark w-fit px-3 py-2 rounded-md hover:bg-navhover"
+          >Features</RouterLink
+        >
+      </nav>
     </nav>
   </header>
 </template>
