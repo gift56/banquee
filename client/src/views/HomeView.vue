3df0d6690d37279d6265deefbe96012cfd51cbed
gift56 <efeasiughu@gmail.com>
2023-10-25 16:13:58 -0400

    working on another section

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,11 +1,13 @@
 <script setup>
 import Hero from "../components/LandingpageComponent/Hero.vue";
 import OneBanking from "../components/LandingpageComponent/OneBanking.vue";
+import SendAndReceive from "../components/LandingpageComponent/SendAndReceive.vue";
 </script>
 
 <template>
   <main>
     <Hero />
     <OneBanking />
+    <SendAndReceive />
   </main>
 </template>
