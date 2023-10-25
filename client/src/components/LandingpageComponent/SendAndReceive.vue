1285fad56a9678ba9fff9ded30132262360108f6
gift56 <efeasiughu@gmail.com>
2023-10-25 16:25:32 -0400

    working on another section

--- client/src/components/LandingpageComponent/SendAndReceive.vue
+++ client/src/components/LandingpageComponent/SendAndReceive.vue
@@ -3,7 +3,11 @@
     <div class="container">
       <div class="sectionCon">
         <div class="flex-1 flex flex-col items-start justify-start gap-6">
-          <h3></h3>
+          <h3 class="headingtext2">Send & receive money instantly</h3>
+          <p class="text-base font-medium text-dark md:text-xl">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et.
+          </p>
         </div>
         <div class="flex-1"></div>
       </div>
