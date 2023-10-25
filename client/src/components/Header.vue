81ea7e35ae51b8114bfaf9ff84d0157ad41c7e94
gift56 <efeasiughu@gmail.com>
2023-10-24 21:07:16 -0400

    working on header

--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -1,11 +1,18 @@
 <script setup>
 import { ref } from "vue";
+import { RouterLink } from "vue-router";
 
 const showNav = ref(false);
 </script>
 
 <template>
   <header class="w-full sticky top-0 z-40">
-    <nav class="px-10 py-4 tab:px-20">hdek</nav>
+    <nav class="headerNav">
+      <RouterLink to="/" class="select-none">
+        <h2 class="font-dm text-2xl font-bold text-primary md:text-2xl">
+          banquee.
+        </h2>
+      </RouterLink>
+    </nav>
   </header>
 </template>
