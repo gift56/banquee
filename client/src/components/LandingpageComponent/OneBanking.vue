4a3a5d10eb7bdfc7f181c38743b41655010fefa3
gift56 <efeasiughu@gmail.com>
2023-10-25 16:00:56 -0400

    working on another section

--- client/src/components/LandingpageComponent/OneBanking.vue
+++ client/src/components/LandingpageComponent/OneBanking.vue
@@ -1,8 +1,14 @@
 <template>
   <section class="w-full bg-white">
     <div class="container">
-      <div class="sectionCon">
-        <div></div>
+      <div class="sectionCon xl:items-start">
+        <div class="flex-1 flex flex-col items-start justify-start gap-6">
+          <h2 class="headingtext">
+            One app. <br class="hidden tab:block" />
+            One banking.
+          </h2>
+        </div>
+        <div class="flex-1"></div>
       </div>
     </div>
   </section>
