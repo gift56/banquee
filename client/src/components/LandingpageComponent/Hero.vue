559e63ec3d01221963e2b83ba9c006270420b53b
gift56 <efeasiughu@gmail.com>
2023-10-25 15:01:58 -0400

    working on hero section

--- client/src/components/LandingpageComponent/Hero.vue
+++ client/src/components/LandingpageComponent/Hero.vue
@@ -3,7 +3,10 @@
     <div class="container">
       <div class="sectionCon">
         <div class="flex-1 flex flex-col items-start justify-start gap-4">
-          <h1></h1>
+          <h1 class="headingtext">
+            Banking <br class="hidden tab:block" />
+            starts here.
+          </h1>
           <p></p>
           <div></div>
           <div></div>
