0ae2943f1819018a17a0955ff1cb96f010daa215
gift56 <efeasiughu@gmail.com>
2023-10-26 16:12:45 -0400

    working on footer

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,10 +1,11 @@
 <script setup>
 import { RouterView } from "vue-router";
 import Header from "./components/Header.vue";
+import Footer from "./components/Footer.vue";
 </script>
 
 <template>
   <Header />
   <RouterView />
-  <footer></footer>
+  <Footer />
 </template>
