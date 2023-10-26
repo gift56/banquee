0a02851fcf74820fea0a9ea0b9bbb32384ef23af
gift56 <efeasiughu@gmail.com>
2023-10-26 14:42:29 -0400

    working on another section

--- client/src/components/LandingpageComponent/BankingApp.vue
+++ client/src/components/LandingpageComponent/BankingApp.vue
@@ -1,7 +1,9 @@
 <template>
   <section class="w-full bg-white py-16">
     <div class="container">
-      <div class="w-full "></div>
+      <div class="w-full bg-primary text-white flex flex-col tab:flex-row items-start gap-8 justify-between px-4">
+        <div></div>
+      </div>
     </div>
   </section>
 </template>
