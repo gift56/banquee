9dee25a407549844a724f9231c341aafca868092
gift56 <efeasiughu@gmail.com>
2023-10-26 15:56:38 -0400

    working on another section

--- client/src/components/LandingpageComponent/Blog.vue
+++ client/src/components/LandingpageComponent/Blog.vue
@@ -1,6 +1,13 @@
 <template>
   <section class="w-full bg-white">
-    <div class="container"></div>
+    <div class="container">
+      <div class="w-full flex flex-col items-start justify-start gap-4 py-16">
+        <div class="w-full flex items-center justify-between gap-6">
+          <h3 class="headingtext2">Blog</h3>
+          
+        </div>
+      </div>
+    </div>
   </section>
 </template>
 
