f2479c7578670aad55e728dfc8348dfcf509d7c7
gift56 <efeasiughu@gmail.com>
2023-10-26 16:32:31 -0400

    working on footer

--- client/src/components/Footer.vue
+++ client/src/components/Footer.vue
@@ -12,7 +12,7 @@
           </RouterLink>
         </div>
         <div
-          class="w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6"
+          class="w-full grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6"
         >
           <div
             v-for="footerLink in footerLinks"
