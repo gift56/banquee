e404103d757c7c0e1fe2abb86536e5e636f0dbde
gift56 <efeasiughu@gmail.com>
2023-10-26 13:51:08 -0400

    working on another section

--- client/src/components/LandingpageComponent/Partner.vue
+++ client/src/components/LandingpageComponent/Partner.vue
@@ -1,7 +1,15 @@
 <template>
   <section class="w-full bg-white">
     <div class="container">
-      
+      <div class="w-full flex flex-col items-center justify-center gap-4 py-16">
+        <h6 class="text-base font-medium text-dark md:text-xl font-dm">
+          Account
+        </h6>
+        <h2 class="headingtext2 text-center">
+          Perfect card <br class="hidden tab:block" />
+          for your needs.
+        </h2>
+      </div>
     </div>
   </section>
 </template>
