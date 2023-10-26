f8b6a6af64a7c3084d5217d2ffb05e0ba5bc49f3
gift56 <efeasiughu@gmail.com>
2023-10-26 14:40:49 -0400

    working on another section

--- client/src/components/LandingpageComponent/BankingApp.vue
+++ client/src/components/LandingpageComponent/BankingApp.vue
@@ -1,5 +1,9 @@
 <template>
-  <div>Banking app</div>
+  <section class="w-full bg-white py-16">
+    <div class="container">
+      <div class="w-full "></div>
+    </div>
+  </section>
 </template>
 
 <script setup></script>
