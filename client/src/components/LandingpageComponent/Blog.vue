7f43b63ec24ad376d36561994e372ec3b004d065
gift56 <efeasiughu@gmail.com>
2023-10-26 15:54:39 -0400

    working on another section

--- client/src/components/LandingpageComponent/Blog.vue
+++ client/src/components/LandingpageComponent/Blog.vue
@@ -1,5 +1,7 @@
 <template>
-  <section class="w-full bg-white">Blog</section>
+  <section class="w-full bg-white">
+    <div class="container"></div>
+  </section>
 </template>
 
 <script setup></script>
