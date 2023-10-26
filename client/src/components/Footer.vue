fbe77e6d915e0fd911144e90b1b4a1b5bd1c99e9
gift56 <efeasiughu@gmail.com>
2023-10-26 16:27:23 -0400

    working on footer

--- client/src/components/Footer.vue
+++ client/src/components/Footer.vue
@@ -4,12 +4,25 @@
       <div
         class="w-full flex flex-col md:flex-row gap-6 items-start justify-between"
       >
-        <div class="flex flex-col items-start justify-start">
+        <div class="flex flex-col items-start justify-start w-full">
           <RouterLink to="/" class="select-none">
             <h2 class="font-dm text-2xl font-bold text-primary md:text-2xl">
               banquee.
             </h2>
           </RouterLink>
+        </div>
+        <div
+          class="w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6"
+        >
+          <div
+            v-for="footerLink in footerLinks"
+            :key="footerLink.title"
+            class="w-full flex flex-col items-start justify-start gap-8"
+          >
+            <h4 class="text-base font-medium text-dark font-dm md:text-lg">
+              {{ footerLink.title }}
+            </h4>
+          </div>
         </div>
       </div>
     </div>
@@ -17,5 +30,6 @@
 </template>
 
 <script setup>
-import { RouterLink } from 'vue-router';
+import { RouterLink } from "vue-router";
+import { footerLinks } from "../utils/constant";
 </script>
