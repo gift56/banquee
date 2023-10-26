99e312d1f8ec767e051d80ab097a05ce5f4da639
gift56 <efeasiughu@gmail.com>
2023-10-26 15:09:01 -0400

    working on another section

--- client/src/components/LandingpageComponent/Faq.vue
+++ client/src/components/LandingpageComponent/Faq.vue
@@ -2,8 +2,15 @@
   <section class="w-full bg-white">
     <div class="container">
       <div class="sectionCon">
-        <div class="w-full tab:flex-1 flex flex-col items-start justify-start gap-4"></div>
-        <div class="w-full tab:flex-1 flex flex-col items-start justify-start gap-4"></div>
+        <div
+          class="w-full tab:flex-1 flex flex-col items-start justify-start gap-4"
+        >
+          <h2 class="headingtext2">Need help?</h2>
+          <div></div>
+        </div>
+        <div
+          class="w-full tab:flex-1 flex flex-col items-start justify-start gap-4"
+        ></div>
       </div>
     </div>
   </section>
