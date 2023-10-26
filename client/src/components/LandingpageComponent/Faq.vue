a876f1cab5a77dddd8e7d63f7eecfa87d1d82fe1
gift56 <efeasiughu@gmail.com>
2023-10-26 15:08:16 -0400

    working on another section

--- client/src/components/LandingpageComponent/Faq.vue
+++ client/src/components/LandingpageComponent/Faq.vue
@@ -1,7 +1,10 @@
 <template>
   <section class="w-full bg-white">
     <div class="container">
-      
+      <div class="sectionCon">
+        <div class="w-full tab:flex-1 flex flex-col items-start justify-start gap-4"></div>
+        <div class="w-full tab:flex-1 flex flex-col items-start justify-start gap-4"></div>
+      </div>
     </div>
   </section>
 </template>
