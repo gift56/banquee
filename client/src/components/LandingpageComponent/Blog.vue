6d71a1548923c375ffee37686b75273cfb7b1c29
gift56 <efeasiughu@gmail.com>
2023-10-26 15:57:40 -0400

    working on another section

--- client/src/components/LandingpageComponent/Blog.vue
+++ client/src/components/LandingpageComponent/Blog.vue
@@ -4,11 +4,24 @@
       <div class="w-full flex flex-col items-start justify-start gap-4 py-16">
         <div class="w-full flex items-center justify-between gap-6">
           <h3 class="headingtext2">Blog</h3>
-          
+          <RouterLink to="/" class="group">
+            <div class="text-primary flex items-center gap-2">
+              All Features
+              <img
+                src="@/assets/icons/right_arrowIcon.svg"
+                alt="arrow"
+                class="group-hover:translate-x-3 transition-all duration-300"
+              />
+            </div>
+          </RouterLink>
         </div>
       </div>
     </div>
   </section>
 </template>
 
-<script setup></script>
+<script setup>
+import { RouterLink } from 'vue-router';
+
+
+</script>
