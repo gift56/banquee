9a83195fb30c270140a89ff17df83e52abd6c3a0
gift56 <efeasiughu@gmail.com>
2023-10-26 14:15:37 -0400

    working on another section

--- client/src/components/LandingpageComponent/Testimony.vue
+++ client/src/components/LandingpageComponent/Testimony.vue
@@ -1,7 +1,11 @@
 <template>
   <section class="w-full bg-white">
     <div class="container">
-      
+      <div class="w-full flex flex-col items-start justify-start gap-4 py-16">
+        <h6 class="text-base font-medium text-dark md:text-xl font-dm">
+          Testimonials
+        </h6>
+      </div>
     </div>
   </section>
 </template>
