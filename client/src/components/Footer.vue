16d408f2d543c791105e36599e45f40e76be497c
gift56 <efeasiughu@gmail.com>
2023-10-26 16:22:56 -0400

    working on footer

--- client/src/components/Footer.vue
+++ client/src/components/Footer.vue
@@ -1,5 +1,21 @@
 <template>
-  <footer class="bg-white py-10">Footer</footer>
+  <footer class="bg-white py-10">
+    <div class="container">
+      <div
+        class="w-full flex flex-col md:flex-row gap-6 items-start justify-between"
+      >
+        <div class="flex flex-col items-start justify-start">
+          <RouterLink to="/" class="select-none">
+            <h2 class="font-dm text-2xl font-bold text-primary md:text-2xl">
+              banquee.
+            </h2>
+          </RouterLink>
+        </div>
+      </div>
+    </div>
+  </footer>
 </template>
 
-<script setup></script>
+<script setup>
+import { RouterLink } from 'vue-router';
+</script>
