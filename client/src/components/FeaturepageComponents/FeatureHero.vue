af6414ee04f16ac55375f360104a8d5d900207b4
gift56 <efeasiughu@gmail.com>
2023-10-28 11:41:55 -0400

    working on feature page

--- client/src/components/FeaturepageComponents/FeatureHero.vue
+++ client/src/components/FeaturepageComponents/FeatureHero.vue
@@ -1,5 +1,9 @@
 <template>
-  <section class="w-full tab:h-screen tab:overflow-hidden bg-featureBg flex items-center justify-center">Hero</section>
+  <section
+    class="w-full tab:h-screen tab:overflow-hidden bg-featureBg flex items-center justify-center flex-col"
+  >
+    <div class="flex flex-col items-center justify-center gap-6 w-[90%] mx-auto"></div>
+  </section>
 </template>
 
 <script setup></script>
