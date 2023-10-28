9e578e48f9f3d5f502777502a3d18c4f49b45034
gift56 <efeasiughu@gmail.com>
2023-10-28 11:42:14 -0400

    working on feature page

--- client/src/components/FeaturepageComponents/FeatureHero.vue
+++ client/src/components/FeaturepageComponents/FeatureHero.vue
@@ -2,7 +2,11 @@
   <section
     class="w-full tab:h-screen tab:overflow-hidden bg-featureBg flex items-center justify-center flex-col"
   >
-    <div class="flex flex-col items-center justify-center gap-6 w-[90%] mx-auto"></div>
+    <div
+      class="flex flex-col items-center justify-center gap-6 w-[90%] mx-auto"
+    >
+      <h4>Features</h4>
+    </div>
   </section>
 </template>
 
