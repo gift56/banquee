3e92bfa7b89e775c16de935735c9738ee49277a4
gift56 <efeasiughu@gmail.com>
2023-10-28 11:44:06 -0400

    working on feature page

--- client/src/components/FeaturepageComponents/FeatureHero.vue
+++ client/src/components/FeaturepageComponents/FeatureHero.vue
@@ -5,7 +5,8 @@
     <div
       class="flex flex-col items-center justify-center gap-6 w-[90%] mx-auto"
     >
-      <h4 class="text-lg text-dark font-medium font-dm md:text-xl">Features</h4>
+      <h4 class="text-lg text-dark font-medium font-dm md:text-xl text-center">Features</h4>
+      <h2 class="headingtext text-center">All in one card.</h2>
     </div>
   </section>
 </template>
