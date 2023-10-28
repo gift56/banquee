e4ba5d9f5cf02b5527ab80c89439c17032371181
gift56 <efeasiughu@gmail.com>
2023-10-28 11:54:57 -0400

    working on feature page

--- client/src/views/FeatureView.vue
+++ client/src/views/FeatureView.vue
@@ -1,9 +1,11 @@
 <template>
   <main>
     <FeatureHero />
+    <FeatureTransaction />
   </main>
 </template>
 
 <script setup>
 import FeatureHero from "../components/FeaturepageComponents/FeatureHero.vue";
+import FeatureTransaction from "../components/FeaturepageComponents/FeatureTransaction.vue";
 </script>
