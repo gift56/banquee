5d841b999d4720584ff63746548eb55e335a34f4
gift56 <efeasiughu@gmail.com>
2023-10-29 15:56:29 -0400

    working  on the next section

--- client/src/components/FeaturepageComponents/FeatureBank.vue
+++ client/src/components/FeaturepageComponents/FeatureBank.vue
@@ -1,5 +1,9 @@
 <template>
-  <section class="w-full">Bank</section>
+  <section class="w-full">
+    <div class="container">
+      
+    </div>
+  </section>
 </template>
 
 <script setup></script>
