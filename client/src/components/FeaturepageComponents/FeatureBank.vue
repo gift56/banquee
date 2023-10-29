f94593eb07e9c7019d04d0eb62a71848a6fa0849
gift56 <efeasiughu@gmail.com>
2023-10-29 16:14:29 -0400

    working  on the next section

--- client/src/components/FeaturepageComponents/FeatureBank.vue
+++ client/src/components/FeaturepageComponents/FeatureBank.vue
@@ -34,18 +34,21 @@
             />
           </div>
           <div
-            class="w-full tab:flex-1 flex flex-col items-center justify-start gap-5 h-[370px] overflow-hidden rounded-[20px] bg-navhover pt-10 px-4 relative"
+            class="w-full tab:flex-1 flex flex-col items-center justify-start gap-5 h-[370px] overflow-hidden rounded-[20px] bg-navhover pt-10 relative"
           >
             <h3
               class="text-2xl text-center md:text-3xl tab:text-4xl font-dm font-medium text-dark"
             >
-              Statistics
+              Cards
             </h3>
             <p
               class="text-base font-medium text-dark/50 text-center md:text-lg tab:max-w-[80%]"
             >
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do.
             </p>
+            <div class="w-full flex items-end justify-between gap-5">
+              
+            </div>
           </div>
         </div>
       </div>
