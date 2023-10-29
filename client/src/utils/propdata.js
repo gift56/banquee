2253bdd9132d9e81a0a763c93eb837d28720a087
gift56 <efeasiughu@gmail.com>
2023-10-29 10:56:50 -0400

    working  on the next section

--- client/src/utils/propdata.js
+++ client/src/utils/propdata.js
@@ -3,4 +3,8 @@ import phone2Image from "../assets/images/phoneManage.svg";
 import phone3Image from "../assets/images/phoneControl.svg";
 import phone4Image from "../assets/images/phoneFeature.svg";
 
-export const sendAndReceivePropData = {};
+export const sendAndReceivePropData = [
+  {
+    headline: "Transactions",
+  },
+];
