7cd34cf14b16bc403c8157057ef0a2bde6e1971d
gift56 <efeasiughu@gmail.com>
2023-10-29 15:34:23 -0400

    working  on the next section

--- client/src/views/FeatureView.vue
+++ client/src/views/FeatureView.vue
@@ -4,10 +4,12 @@
     <section class="w-full" v-for="data in PropData">
       <FeatureTransaction :data="data" />
     </section>
+    <FeatureBank />
   </main>
 </template>
 
 <script setup>
+import FeatureBank from "../components/FeaturepageComponents/FeatureBank.vue";
 import FeatureHero from "../components/FeaturepageComponents/FeatureHero.vue";
 import FeatureTransaction from "../components/FeaturepageComponents/FeatureTransaction.vue";
 import { PropData } from "../utils/propdata";
