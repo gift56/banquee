5819d63b76c835edac42c60ae5622c98d09c535e
gift56 <efeasiughu@gmail.com>
2023-10-29 10:12:07 -0400

    working  on the next section

--- client/src/components/FeaturepageComponents/FeatureTransaction.vue
+++ client/src/components/FeaturepageComponents/FeatureTransaction.vue
@@ -1,5 +1,16 @@
 <template>
-  <section>Transaction</section>
+  <section class="w-full bg-white">
+    <div class="container">
+      <div class="sectionCon">
+        <div
+          class="w-full tab:flex-1 flex flex-col items-start justify-start gap-5"
+        >
+          <h4>Transactions</h4>
+        </div>
+        <div class="w-full tab:flex-1"></div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script setup></script>
