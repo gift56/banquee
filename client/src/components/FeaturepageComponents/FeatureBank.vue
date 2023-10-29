27890ba2a6cf1648b436e9030fe75e63fcdd4f60
gift56 <efeasiughu@gmail.com>
2023-10-29 15:59:14 -0400

    working  on the next section

--- client/src/components/FeaturepageComponents/FeatureBank.vue
+++ client/src/components/FeaturepageComponents/FeatureBank.vue
@@ -1,7 +1,9 @@
 <template>
   <section class="w-full">
     <div class="container">
-      
+      <div class="w-full flex flex-col items-center justify-center gap-4 py-16">
+
+      </div>
     </div>
   </section>
 </template>
