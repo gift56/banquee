218e2ecdf4ec573b940ddbcade9eb182bc12946c
gift56 <efeasiughu@gmail.com>
2023-10-29 15:59:53 -0400

    working  on the next section

--- client/src/components/FeaturepageComponents/FeatureBank.vue
+++ client/src/components/FeaturepageComponents/FeatureBank.vue
@@ -1,8 +1,10 @@
 <template>
   <section class="w-full">
     <div class="container">
-      <div class="w-full flex flex-col items-center justify-center gap-4 py-16">
-
+      <div class="w-full flex flex-col items-center justify-center gap-6 py-16">
+        <div>
+          <h2>All in one bank. Really.</h2>
+        </div>
       </div>
     </div>
   </section>
