4e378f0915cf939458a5d0e0dbc9454d1d8bb25c
gift56 <efeasiughu@gmail.com>
2023-10-29 10:12:48 -0400

    working  on the next section

--- client/src/components/FeaturepageComponents/FeatureTransaction.vue
+++ client/src/components/FeaturepageComponents/FeatureTransaction.vue
@@ -5,7 +5,10 @@
         <div
           class="w-full tab:flex-1 flex flex-col items-start justify-start gap-5"
         >
-          <h4>Transactions</h4>
+          <h4 class="text-base font-medium font-dm text-dark md:text-xl">
+            Transactions
+          </h4>
+          <h2 class="headingtext2">Send & receive money instantly</h2>
         </div>
         <div class="w-full tab:flex-1"></div>
       </div>
