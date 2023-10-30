5178131ce39043e67beccd99fea36b329e512332
gift56 <efeasiughu@gmail.com>
2023-10-30 09:17:58 -0400

    working  on the next section

--- client/src/components/LandingpageComponent/Intergration.vue
+++ client/src/components/LandingpageComponent/Intergration.vue
@@ -2,7 +2,9 @@
   <section class="w-full bg-white">
     <div class="container">
         <div class="w-full flex flex-col items-start justify-start gap-8 py-16">
-            
+            <div class="w-full flex flex-wrap items-center justify-between gap-6">
+                <img src="" alt="" v-for="clientImage in ">
+            </div>
         </div>
     </div>
   </section>
