76efad5c2a988ab0ff5957e71718f13409750e78
gift56 <efeasiughu@gmail.com>
2023-10-30 09:30:48 -0400

    working  on the next section

--- client/src/components/LandingpageComponent/Intergration.vue
+++ client/src/components/LandingpageComponent/Intergration.vue
@@ -1,13 +1,22 @@
 <template>
   <section class="w-full bg-white">
     <div class="container">
-        <div class="w-full flex flex-col items-start justify-start gap-8 py-16">
-            <div class="w-full flex flex-wrap items-center justify-between gap-6">
-                <img src="" alt="" v-for="clientImage in ">
-            </div>
+      <div class="w-full flex flex-col items-start justify-start gap-8 py-16">
+        <div
+          class="w-full flex flex-wrap gap-x-6 gap-y-3 items-center justify-center md:items-start md:justify-start tab:max-w-[50%]"
+        >
+          <img
+            :src="clientImage"
+            alt="client_photo"
+            class="w-[86px] aspect-video object-contain"
+            v-for="clientImage in clientsImages"
+          />
         </div>
+      </div>
     </div>
   </section>
 </template>
 
-<script setup></script>
+<script setup>
+import { clientsImages } from "../../utils/constant";
+</script>
