1644bcf173cb7ee328df64fa99bd77cf8ceee785
gift56 <efeasiughu@gmail.com>
2023-10-30 09:37:39 -0400

    working  on the next section

--- client/src/components/FeaturepageComponents/FeatureBank.vue
+++ client/src/components/FeaturepageComponents/FeatureBank.vue
@@ -79,15 +79,45 @@
             <p
               class="text-base font-medium text-dark/50 text-center md:text-lg tab:max-w-[80%]"
             >
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
             </p>
           </div>
           <div
             class="w-full flex flex-col items-center justify-start gap-5 h-[570px] overflow-hidden rounded-[20px] bg-navhover pt-10 px-4 relative"
-          ></div>
+          >
+            <div
+              class="w-full flex flex-col items-center justify-start gap-5 h-[570px] overflow-hidden rounded-[20px] bg-navhover pt-10 px-4 relative"
+            >
+              <h3
+                class="text-2xl text-center md:text-3xl tab:text-4xl font-dm font-medium text-dark"
+              >
+                Saving accounts
+              </h3>
+              <p
+                class="text-base font-medium text-dark/50 text-center md:text-lg tab:max-w-[80%]"
+              >
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              </p>
+            </div>
+          </div>
           <div
             class="w-full flex flex-col items-center justify-start gap-5 h-[570px] overflow-hidden rounded-[20px] bg-navhover pt-10 px-4 relative"
-          ></div>
+          >
+            <div
+              class="w-full flex flex-col items-center justify-start gap-5 h-[570px] overflow-hidden rounded-[20px] bg-navhover pt-10 px-4 relative"
+            >
+              <h3
+                class="text-2xl text-center md:text-3xl tab:text-4xl font-dm font-medium text-dark"
+              >
+                Instant transactions
+              </h3>
+              <p
+                class="text-base font-medium text-dark/50 text-center md:text-lg tab:max-w-[80%]"
+              >
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
