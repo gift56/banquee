f917a19b19221af70627d0d632eb96c8a67342ca
gift56 <efeasiughu@gmail.com>
2023-10-31 10:18:47 -0400

    working  on the next section

--- client/src/components/FeaturepageComponents/FeatureCard.vue
+++ client/src/components/FeaturepageComponents/FeatureCard.vue
@@ -1,6 +1,12 @@
 <template>
-    <section>Card</section>
-  </template>
-  
-  <script setup></script>
-  
+  <section class="w-full bg-white">
+    <div class="container">
+      <div
+        class="w-full flex flex-col items-center justify-center gap-4 py-16"
+      >
+    </div>
+    </div>
+  </section>
+</template>
+
+<script setup></script>
