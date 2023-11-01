0fc61b42c32d598be98fb720266f0b094647661c
gift56 <efeasiughu@gmail.com>
2023-11-01 07:04:10 -0400

    working  on the next section

--- client/src/views/CompareView.vue
+++ client/src/views/CompareView.vue
@@ -3,7 +3,7 @@
     <section class="w-full bg-featureBg">
       <div class="container">
         <div
-          class="w-full flex flex-col tab:flex-row items-center justify-between gap-6 pt-8"
+          class="w-full flex flex-col tab:flex-row items-center justify-between gap-6 pt-10"
         >
           <div
             class="w-full tab:flex-1 flex flex-col items-start justify-start gap-6"
@@ -11,10 +11,13 @@
             <h6 class="text-base font-medium text-dark md:text-xl font-dm">
               Compare Cards
             </h6>
-            <h2 class="headingtext">The ideal <br class="hidden lg:block"> card for you</h2>
+            <h2 class="headingtext">
+              The ideal <br class="hidden lg:block" />
+              card for you
+            </h2>
           </div>
           <div class="w-full tab:flex-1">
-            <img src="../assets/images/ideaHero.svg" alt="">
+            <img src="../assets/images/ideaHero.svg" alt="heroImage" />
           </div>
         </div>
       </div>
