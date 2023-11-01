f9978180476329699034c313b04e54b77ff050c6
gift56 <efeasiughu@gmail.com>
2023-11-01 06:56:28 -0400

    working  on the next section

--- client/src/views/CompareView.vue
+++ client/src/views/CompareView.vue
@@ -2,9 +2,16 @@
   <main>
     <section class="w-full bg-featureBg">
       <div class="container">
-        <div class="w-full flex flex-col tab:flex-row items-center justify-between gap-6 pt-8">
-          <div class="w-full tab:flex-1 flex">
-
+        <div
+          class="w-full flex flex-col tab:flex-row items-center justify-between gap-6 pt-8"
+        >
+          <div
+            class="w-full tab:flex-1 flex flex-col items-start justify-start gap-6"
+          >
+            <h6 class="text-base font-medium text-dark md:text-xl font-dm">
+              Compare Cards
+            </h6>
+            <h2 class="headingtext">The ideal card for you</h2>
           </div>
           <div class="w-full tab:flex-1"></div>
         </div>
