9165e62237477a8e716dde5dcec19f441a624297
gift56 <efeasiughu@gmail.com>
2023-11-01 07:13:02 -0400

    working  on the next section

--- client/src/views/CompareView.vue
+++ client/src/views/CompareView.vue
@@ -82,7 +82,7 @@
             :key="index"
             class="w-full flex flex-col items-start justify-start gap-3"
           >
-            <h3 class="">{{ itemInfo.title }}</h3>
+            <h3 class="text-base font-medium text-dark md:text-xl font-dm">{{ itemInfo.title }}</h3>
           </div>
         </div>
       </div>
