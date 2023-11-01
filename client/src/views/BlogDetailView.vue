766027c1767dab851a14d47f1d3f91800feefebd
gift56 <efeasiughu@gmail.com>
2023-11-01 10:10:19 -0400

    added active state

--- client/src/views/BlogDetailView.vue
+++ client/src/views/BlogDetailView.vue
@@ -2,10 +2,15 @@
   <main>
     <section class="w-full">
       <div class="container">
-        <div></div>
+        <div class="w-full flex flex-col items-center justify-center gap-6">
+          <h2>{{}}</h2>
+        </div>
       </div>
     </section>
   </main>
 </template>
 
-<script></script>
+<script>
+import { blogData } from "../utils/constant";
+
+</script>
