fdeead764720d649a999bc0ae1f81974d49e0b80
gift56 <efeasiughu@gmail.com>
2023-11-01 10:41:43 -0400

    added active state

--- client/src/views/BlogDetailView.vue
+++ client/src/views/BlogDetailView.vue
@@ -2,17 +2,30 @@
   <main>
     <section class="w-full">
       <div class="container">
-        <div class="w-full flex flex-col items-center justify-center gap-6">
-          <h2>{{}}</h2>
+        <div
+          class="w-full flex flex-col items-center justify-center gap-6 py-16"
+        >
+          <h2 class="headingtext2 text-center tab:max-w-[70%]">
+            {{ blogDetail.blogHeadline }}
+          </h2>
+          <p class="text-center text-lg md:text-xl font-medium tab:max-w-[600px]">{{ blogDetail.detail }}</p>
+          <img :src="blogDetail.blogImage" :alt="blogDetail.blogHeadline">
         </div>
       </div>
     </section>
   </main>
 </template>
 
-<script setup>
+<script>
 import { blogData } from "../utils/constant";
-const blogDetail = blogData.find((item) => item.blogid === $route.params.blogId);
 
-console.log(blogDetail);
+export default {
+  data() {
+    return {
+      blogDetail: blogData.find(
+        (item) => item.blogid === this.$route.params.blogId
+      ),
+    };
+  },
+};
 </script>
