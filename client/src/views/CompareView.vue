83dccfdc7aa0006a507d820de41a7679dd99e991
gift56 <efeasiughu@gmail.com>
2023-11-01 06:45:43 -0400

    working  on the next section

--- client/src/views/CompareView.vue
+++ client/src/views/CompareView.vue
@@ -1,5 +1,11 @@
 <template>
-  <main>Compare</main>
+  <main>
+    <section class="w-full bg-featureBg">
+      <div class="container">
+        <div class="w-full flex flex-col tab:flex-row items-center justify-between gap-6"></div>
+      </div>
+    </section>
+  </main>
 </template>
 
 <script setup></script>
