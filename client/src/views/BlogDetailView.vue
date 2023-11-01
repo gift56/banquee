fa89efdb05f978b7c66b06851a44b62fed8dbc89
gift56 <efeasiughu@gmail.com>
2023-11-01 10:45:53 -0400

    added active state

--- client/src/views/BlogDetailView.vue
+++ client/src/views/BlogDetailView.vue
@@ -8,8 +8,22 @@
           <h2 class="headingtext2 text-center tab:max-w-[70%]">
             {{ blogDetail.blogHeadline }}
           </h2>
-          <p class="text-center text-lg md:text-xl font-medium tab:max-w-[600px]">{{ blogDetail.detail }}</p>
-          <img :src="blogDetail.blogImage" :alt="blogDetail.blogHeadline">
+          <p
+            class="text-center text-lg md:text-xl font-medium tab:max-w-[600px] text-dark"
+          >
+            {{ blogDetail.detail }}
+          </p>
+          <img
+            :src="blogDetail.blogImage"
+            :alt="blogDetail.blogHeadline"
+            class="tab:w-[68%] aspect-video rounded-2xl object-contain"
+          />
+
+          <div
+            class="flex flex-col items-start justify-start w-full tab:max-w-[600px] tab:w-[600px]"
+          >
+            <h4  class="text-lg md:text-2xl font-medium text-dark">Et malesuada fames ac turpis</h4>
+          </div>
         </div>
       </div>
     </section>
