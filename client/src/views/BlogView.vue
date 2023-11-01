991bd89691ee3f3056014e1497ab35d0a177c830
gift56 <efeasiughu@gmail.com>
2023-11-01 07:53:55 -0400

    working  on the blog page

--- client/src/views/BlogView.vue
+++ client/src/views/BlogView.vue
@@ -1,11 +1,11 @@
 <template>
   <main class="w-full">
     <div class="container">
-      <div class="w-full flex flex-col items-center justify-center gap-10">
+      <div class="w-full flex flex-col items-center justify-center gap-10 py-16">
         <h2 class="headingtext">Blog</h2>
         <div class="flex flex-col items-start justify-start gap-4 w-full">
           <div class="flex items-center justify-start gap-5">
-            <h4>Categories</h4>
+            <h4 class="text-base font-medium text-dark md:text-lg font-dm">Categories</h4>
           </div>
         </div>
       </div>
